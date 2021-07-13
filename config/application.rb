d5ff5c66a321d260f77b2011115ae46023f1a565
leduyvuong <leduyvuong99@gmail.com>
2021-07-13 17:28:58 +0700

    Implement advanced login

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module RubyonRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.available_locales = [:en, :vi]
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
     config.load_defaults 6.1
     # Configuration for the application, engines, and railties goes here.
