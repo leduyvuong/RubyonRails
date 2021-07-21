7c1622dd66fec70989242c78b42944ed9bec7602
leduyvuong <leduyvuong99@gmail.com>
2021-07-21 15:01:52 +0700

    improve controller and i18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,20 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :find_u, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.paginate(page: params[:page])   
   end
+  def find_u
+    @user = User.find(params[:id])  
+    if @user
+      return @user
+    else
+      flash[:danger] = t('static_pages.home.danger')
+      return root_url
+    end
+  end
   def show
-    @user = User.find(params[:id])   
   end
   def new
     @user = User.new
@@ -18,25 +27,28 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render "new"
     end
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = t('static_pages.home.update_pf')
       redirect_to @user
     else
-      render 'edit'
+      render "edit"
     end
   end
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    if @user.destroy
+      flash[:success] = t('users.index.destroy_S')
+      redirect_to users_url
+    else
+      flash[:danger] = t('users.index.destroy_F')
+      redirect_to users_url
+    end
   end
   
   private
@@ -47,7 +59,7 @@ class UsersController < ApplicationController
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = t('static_pages.home.danger')
         redirect_to login_url
       end
     end
